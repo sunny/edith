7e9dc5ee9ac9ea5518bc298f1f958571d4b2dce7
sunny <negatif@gmail.com>
2008-03-18 09:33:30 +0100

    New version of Prototype

--- includes/script.js
+++ includes/script.js
@@ -1,7 +1,9 @@
 document.observe('dom:loaded', function() {
   if ($('submit','text','save').all()) {
-    $('submit').hide();
     $('text').focus();
-    new Form.Observer('save', 2, function() { $('save').request() });
+    $('submit').hide();
+    new Form.Observer('save', 2, function(form) {
+      form.request()
+    });
   }
 });
