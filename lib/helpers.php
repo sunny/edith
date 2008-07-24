ef213b4c8f2725a59e306fe004bd2935d6ac60b6
Sunny Ripert <negatif@gmail.com>
2008-07-24 15:19:48 +0200

    Removing code feels good

--- lib/helpers.php
+++ lib/helpers.php
@@ -1,17 +1,7 @@
 <?php
 
-function ends_with($name, $end) {
-  return $name == $end or 0 === strpos(strrev($name), strrev($end));
-}
-
 function h($string) {
   return htmlspecialchars($string);
-}
-
-function remove_from_end($name, $end) {
-  if (ends_with($name, $end))
-    return substr($name, 0, strlen($name) - strlen($end));
-  return $name;
 }
 
 function request_var($name) {
@@ -19,4 +9,3 @@ function request_var($name) {
     return '';
   return get_magic_quotes_gpc() ? stripslashes($_REQUEST[$name]) : $_REQUEST[$name];
 }
-
