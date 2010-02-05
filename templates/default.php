8ed95edaa73cf0b2ae95d7acb057002ac63e84e5
Sunny Ripert <sunny@sunfox.org>
2010-02-05 15:28:21 +0100

    HTML5 ftw

--- templates/default.php
+++ templates/default.php
@@ -1,16 +1,16 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
   <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
   <title><?php echo h($page->name()); ?></title>
-  <link rel="shortcut icon" type="image/png" href="<?php echo EDITH_URI ?>/public/icon.png" />
-  <link type="text/css" rel="stylesheet" href="<?php echo EDITH_URI ?>/public/style.css" />
-  <script type="text/javascript" src="<?php echo EDITH_URI ?>/public/prototype.js"></script>
-  <script type="text/javascript" src="http://edith-mobwrite.appspot.com/static/compressed_form.js"></script>
-  <script type="text/javascript" src="<?php echo EDITH_URI ?>/public/script.js"></script>
+  <link rel="shortcut icon" href="<?php echo EDITH_URI ?>/public/icon.png" />
+  <link rel="stylesheet" href="<?php echo EDITH_URI ?>/public/style.css" />
+  <script src="<?php echo EDITH_URI ?>/public/prototype.js"></script>
+  <script src="http://edith-mobwrite.appspot.com/static/compressed_form.js"></script>
+  <script src="<?php echo EDITH_URI ?>/public/script.js"></script>
 </head>
 <body>
+
   <form method="post" action="<?php echo EDITH_URI ?>/<?php echo h($page->name) ?>" id="save">
     <p>
       <textarea name="text" id="edith-text-<?php echo $page->name ?>" cols="42" rows="42"><?php echo h($page->text) ?></textarea>
@@ -20,11 +20,13 @@
 
 <?php if (defined('MOBWRITE_URI') and defined('MOBWRITE_KEY')
     and MOBWRITE_URI and MOBWRITE_KEY) : ?>
+
   <script type="text/javascript">
     mobwrite.syncGateway='<?php echo h(MOBWRITE_URI) ?>';
     mobwrite.share('<?php echo MOBWRITE_KEY.'-'.$page->name ?>')
   </script>
 <?php endif; ?>
+
 
 </body>
 </html>
--- templates/html.php
+++ templates/html.php
@@ -1,12 +1,11 @@
 <?php require 'lib/markdown.php'; ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
   <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
   <title><?php echo h(ucfirst($page->name)); ?></title>
-  <link rel="shortcut icon" type="image/png" href="<?php echo EDITH_URI ?>/public/icon.png" />
-  <style type="text/css">
+  <link rel="shortcut icon" href="<?php echo EDITH_URI ?>/public/icon.png" />
+  <style>
     body {
       padding:1em;
       font:1em Lucida Grande, Helvetica, Arial, sans-serif;
