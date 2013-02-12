d304c7d1e08e2ab5d4dea8446c0cb235bb6a644a
Benjamin Danon <benjamin@sphax3d.org>
2013-02-12 17:28:43 +0100

    Remove the EDITH_URI trailing slash

--- templates/default.php
+++ templates/default.php
@@ -3,15 +3,15 @@
 <head>
   <meta charset="utf-8">
   <title><?php echo h($page->name()); ?></title>
-  <link rel="shortcut icon" href="<?php echo EDITH_URI ?>/public/icon.png" />
-  <link rel="stylesheet" href="<?php echo EDITH_URI ?>/public/style.css" />
+  <link rel="shortcut icon" href="<?php echo rtrim(EDITH_URI, '/') ?>/public/icon.png" />
+  <link rel="stylesheet" href="<?php echo rtrim(EDITH_URI, '/') ?>/public/style.css" />
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
   <script src="http://edith-mobwrite.appspot.com/static/compressed_form.js"></script>
-  <script src="<?php echo EDITH_URI ?>/public/script.js"></script>
+  <script src="<?php echo rtrim(EDITH_URI, '/') ?>/public/script.js"></script>
 </head>
 <body>
 
-  <form method="post" action="<?php echo EDITH_URI ?>/<?php echo h($page->name) ?>">
+  <form method="post" action="<?php echo rtrim(EDITH_URI, '/') ?>/<?php echo h($page->name) ?>">
     <textarea name="text" cols="42" rows="42" autocomplete="off"><?php echo h($page->text) ?></textarea>
     <input type="submit" />
   </form>
