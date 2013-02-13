e0c85837c28be8cc19d8c84ee0b7b707c7e1aeac
Sunny Ripert <sunny@sunfox.org>
2013-02-13 21:15:32 +0100

    Reverts "Small html fixes" and fixes #12
    This reverts commit af802e336c16ba9b8a057f5a110b30fa45e28c0a.
    
    Conflicts:
    	templates/default.php

--- templates/default.php
+++ templates/default.php
@@ -13,17 +13,19 @@
 </head>
 <body>
 
-  <form method="post" action="<?php echo EDITH_URI ?>/<?php echo h($page->name) ?>">
-    <textarea name="text" cols="42" rows="42" autocomplete="off"><?php echo h($page->text) ?></textarea>
+  <form method="post" action="<?php echo EDITH_URI ?>/<?php echo h($page->name) ?>" id="save">
+    <textarea name="text" id="edith-text-<?php echo $page->name ?>" cols="42" rows="42" autocomplete="off"><?php echo h($page->text) ?></textarea>
     <input type="submit" />
   </form>
 
 <?php if (mobwrite_enabled()) : ?>
+
   <script>
     mobwrite.syncGateway = '<?php echo h(MOBWRITE_URI) ?>'
     mobwrite.share('<?php echo MOBWRITE_KEY ?>-<?php echo $page->name ?>')
   </script>
-
 <?php endif; ?>
+
+
 </body>
 </html>
