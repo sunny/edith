11f3577506c39c01039714f947394605df4361f8
Sunny Ripert <sunny@sunfox.org>
2013-05-18 14:44:03 +0200

    Scripts in footer

--- templates/default.php
+++ templates/default.php
@@ -8,11 +8,6 @@ header('Content-type: text/html; charset="UTF-8"');
   <title><?php echo h($page->name()); ?></title>
   <link rel="shortcut icon" href="<?php echo EDITH_URI ?>/public/icon.png" />
   <link rel="stylesheet" href="<?php echo EDITH_URI ?>/public/style.css" />
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-<?php if (mobwrite_enabled()) : ?>
-  <script src="<?php echo h(MOBWRITE_URI) ?>/static/compressed_form.js"></script>
-<?php endif; ?>
-  <script src="<?php echo EDITH_URI ?>/public/script.js"></script>
 </head>
 <body>
 
@@ -22,7 +17,11 @@ header('Content-type: text/html; charset="UTF-8"');
     <input type="submit" />
   </form>
 
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+  <script src="<?php echo EDITH_URI ?>/public/script.js"></script>
+
 <?php if (mobwrite_enabled()) : ?>
+  <script src="<?php echo h(MOBWRITE_URI) ?>/static/compressed_form.js"></script>
   <script>
     mobwrite.syncGateway = '<?php echo h(MOBWRITE_URI) ?>/scripts/q.py'
     mobwrite.share('text-<?php echo h(MOBWRITE_KEY) ?>-<?php echo h($page->name) ?>')
