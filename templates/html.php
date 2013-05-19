1d13f91d1c2fa506be691d2900c3ac34aa9b3ebf
Sunny Ripert <sunny@sunfox.org>
2013-05-19 18:11:11 +0200

    JS in footer in html template

--- templates/html.php
+++ templates/html.php
@@ -8,12 +8,13 @@ header('Content-type: text/html; charset="UTF-8"');
   <title><?php echo h(ucfirst($page->name)); ?></title>
   <link rel="shortcut icon" href="<?php echo EDITH_URI ?>/public/icon.png" />
   <link rel="stylesheet" href="<?php echo EDITH_URI ?>/public/html.css" />
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-  <script src="<?php echo EDITH_URI ?>/public/html.js"></script>
 </head>
 <body>
 
 <?php echo Markdown($page->text); ?>
 
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+  <script>window.jQuery || document.write('<script src="public/jquery.js"><\/script>')</script>
+  <script src="<?php echo EDITH_URI ?>/public/html.js"></script>
 </body>
 </html>
