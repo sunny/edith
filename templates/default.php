0bfa5e41216fea70b2d79bf8618d94d83ff8bd12
Sunny Ripert <sunny@sunfox.org>
2015-05-06 10:49:56 +0200

    Move jquery to vendor dir

--- templates/default.php
+++ templates/default.php
@@ -19,7 +19,7 @@ header('Content-type: text/html; charset="UTF-8"');
   </form>
 
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="public/jquery.js"><\/script>')</script>
+  <script>window.jQuery || document.write('<script src="public/vendor/jquery.js"><\/script>')</script>
 
   <script src="<?php echo EDITH_URI ?>/public/script.js"></script>
 
--- templates/html.php
+++ templates/html.php
@@ -15,7 +15,7 @@ header('Content-type: text/html; charset="UTF-8"');
 <?php echo MarkdownExtended($page->text); ?>
 
   <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="public/jquery.js"><\/script>')</script>
+  <script>window.jQuery || document.write('<script src="public/vendor/jquery.js"><\/script>')</script>
   <script src="<?php echo EDITH_URI ?>/public/html.js"></script>
 </body>
 </html>
