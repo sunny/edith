473259f71c5ecbc0f5f7bb7ce82a0e3cc3c3b79f
Sunny Ripert <sunny@sunfox.org>
2020-09-24 00:48:19 +0200

    Remove jQuery and coffeescript dependencies

---
+++ public/script.js
@@ -0,0 +1,158 @@
+// Indent using the tab key
+// Via https://stackoverflow.com/a/6140696/311657
+const tabToIndentInputListener = e => {
+  if (e.key != "Tab" || e.ctrlKey || e.shiftKey) return
+
+  e.preventDefault()
+
+  // get caret position/selection
+  const start = this.selectionStart
+  const end = this.selectionEnd
+  const value = this.value
+
+  // set textarea value to: text before caret + tab + text after caret
+  this.value = `${value.substring(0, start)}\t${value.substring(end)}`
+
+  // put caret at right position again (add one for the tab)
+  this.selectionStart = this.selectionEnd = start + 1
+}
+
+// Conditionnally ask for confirmation before closing the window
+// Via https://stackoverflow.com/a/2923258/311657
+// Takes a callback that returns the message to show or nothing.
+const onConfirmClose = callback => {
+  window.onbeforeunload = event => {
+    const message = callback(event)
+    if (!message) return
+    if (typeof event == 'undefined') event = window.event
+    if (event) event.returnValue = message
+    message
+  }
+}
+
+// Returns the data-url for image resembling a page.
+const faviconImage = (text, color) => {
+  const lines = text.split(/\n/).slice(0, 4)
+
+  const canvas = document.createElement("canvas")
+  canvas.width = 16
+  canvas.height = 16
+
+  // Page
+  ctx = canvas.getContext("2d")
+  ctx.rect(1, 1, 14, 14)
+  ctx.fillStyle = "#eee"
+  ctx.strokeStyle = "#333"
+  ctx.stroke()
+  ctx.fill()
+
+  // Lines
+  ctx.strokeStyle = color
+  ctx.beginPath()
+  ctx.ln
+  const linesY = [4, 7, 10, 13]
+  lines.forEach((line, index) => {
+    const length = line.trim().length
+    ctx.moveTo(3, linesY[index])
+    ctx.lineTo(3 + length, linesY[index])
+  })
+  ctx.stroke()
+
+  return canvas.toDataURL("image/png")
+}
+
+class EdithForm {
+  constructor() {
+    this.form = document.getElementById("save");
+    this.textarea = this.form.querySelector("textarea");
+    this.changed = false;
+    this.saving = false;
+
+    this.textarea.addEventListener("input", this.onChange);
+    window.addEventListener("keydown", this.onKeydown);
+    this.textarea.addEventListener("keyup", tabToIndentInputListener);
+    onConfirmClose(this.onConfirmClose);
+
+    this.updateFavicon("black");
+  }
+
+  // Replace the current favicon with the element's text.
+  updateFavicon(color) {
+    const link = document.createElement("link");
+    link.rel = "icon";
+    link.href = faviconImage(this.textarea.value, color);
+    document.querySelector("link[rel~=icon]").remove();
+    document.head.appendChild(link);
+  }
+
+  // Redirect after all changes have been saved
+  // Arguments:
+  // - url: to redirect to
+  // Example:
+  //   this.redirect("/")
+  redirect(url) {
+    if (this.saving) {
+      setTimeout(() => this.redirect(url), 100);
+    } else {
+      window.location = url;
+    }
+  }
+
+  save() {
+    this.saving = true;
+    const text = this.textarea.value;
+    const options = {
+      method: text == "" ? "DELETE" : "PUT",
+      body: new URLSearchParams({ text: text }),
+      headers: {
+        "X-Requested-With": "XMLHttpRequest",
+      },
+    };
+
+    fetch(this.form.action, options)
+      .then(this.onSave)
+      .catch(this.onSaveError)
+  }
+
+  // Events
+
+  onKeydown = (event) => {
+    if (event.ctrlKey && event.key == "e") {
+      this.redirect(window.location.href.replace(/\/$/, "/index") + ".html");
+    }
+  };
+
+  onConfirmClose = () => {
+    if (this.saving || this.changed) {
+      return "Your last modifications didn't get saved yet.";
+    }
+  };
+
+  onChange = () => {
+    this.updateFavicon("grey");
+    this.changed = true;
+    this.checkForChange();
+  };
+
+  onSave = () => {
+    this.saving = false;
+    this.checkForChange();
+    if (!this.saving) this.updateFavicon("black");
+  };
+
+  onSaveError = () => {
+    this.saving = false;
+    this.checkForChange();
+    if (!this.saving) this.updateFavicon("red");
+  };
+
+  checkForChange() {
+    if (this.changed && !this.saving) {
+      this.changed = false;
+      this.save();
+    }
+  }
+}
+
+// Start
+new EdithForm()
--- templates/default.php
+++ templates/default.php
@@ -12,14 +12,22 @@ header('Content-type: text/html; charset="UTF-8"');
 </head>
 <body>
 
-  <form method="post" action="<?php echo EDITH_URI ?>/<?php echo h($page->name) ?>" id="save">
-    <textarea<?php if (mobwrite_enabled()) echo ' id="text-'.h(MOBWRITE_KEY).'-'.h($page->name).'"' ?>
-      name="text" cols="42" rows="42" autocomplete="off"><?php echo h($page->text) ?></textarea>
+  <form
+    method="post"
+    action="<?php echo EDITH_URI ?>/<?php echo h($page->name) ?>"
+    id="save"
+  >
+    <textarea
+      <?php if (mobwrite_enabled()) echo ' id="text-'.h(MOBWRITE_KEY).'-'.h($page->name).'"' ?>
+
+      autofocus
+      name="text"
+      cols="42"
+      rows="42"
+      autocomplete="off"
+    ><?php echo h($page->text) ?></textarea>
     <input type="submit" />
   </form>
-
-  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-  <script>window.jQuery || document.write('<script src="public/vendor/jquery.js"><\/script>')</script>
 
   <script src="<?php echo EDITH_URI ?>/public/script.js"></script>
 
