18e0a8142c753ccd7c7964408561a328e57bad5b
andrew <andrewten45@gmail.com>
2023-09-05 14:36:25 -0500

    Added dedicated 404.php file

---
+++ public/404.php
@@ -0,0 +1,15 @@
+<!-- 404.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>404 Not Found</title>
+    <script src="script.js"></script> <!-- Include js file -->
+</head>
+<body>
+    <!-- Content display -->
+    <h1>404 Not Found</h1>
+    <p>Page not found.</p>
+</body>
+</html>
