aaa97687cfd94684e1fb802ad08676f80f8d738d
andrewten45 <andrewten45@gmail.com>
2023-10-18 11:34:43 -0500

    Added script for reloading page; works as expected

--- lib/404.php
+++ lib/404.php
@@ -7,6 +7,10 @@
 <body>
 	<h1>404 Not Found</h1>
 
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+	<script>window.jQuery || document.write('<script src="public/vendor/jquery.js"><\/script>')</script>
+	<script src="/public/html.js"></script>
+
 	<p>Error Message: <?php echo "Page not found"; ?></p>
 </body>
 </html>
